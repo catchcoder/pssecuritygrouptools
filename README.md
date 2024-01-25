2413eb685ea7ee6983a661f00532a49f11ca38ab
Chris Hawkins <catchcoder@users.noreply.github.com>
2024-01-25 16:12:32 +0000

    Minor change

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# pssecuritygrouptools" 
+# Powershell Security Group Tools
+
--- list-users-in-group.ps1
+++ list-users-in-group.ps1
@@ -1,7 +1,7 @@
 $param1 = $args[0]
 if ( $param1 ) {
     Write-Host "Searching..."
-    Get-ADGroupMember -Identity $param1 -Server ads.bris.ac.uk | Select-Object name, DisplayName
+    Get-ADGroupMember -Identity $param1 -Server ads.bris.ac.uk | Select-Object name
 } else{
-    Write-Host -ForegroundColor green "usage ./list-users-in-group.ps1 ad-security-group e.g. ./list-users-in-group.ps1 'EENG_VIL_Storage_server'"
+    Write-Host -ForegroundColor green "Usage ./list-users-in-group.ps1 ad-security-group e.g. ./list-users-in-group.ps1 'EENG_VIL_Storage_server'"
 }
