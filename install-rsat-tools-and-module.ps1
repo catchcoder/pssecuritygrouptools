17baa515199d446e608e8054139de54dd50d6f4a
Chris Hawkins <catchcoder@users.noreply.github.com>
2024-01-30 16:22:22 +0000

    removed Get-WindowsCapability

--- install-rsat-tools-and-module.ps1
+++ install-rsat-tools-and-module.ps1
@@ -1,5 +1,5 @@
 #Requires -Version 6
 #Requires -RunAsAdministrator
 Add-WindowsCapability -Online -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
-write-host "Verify Ative Directory is p"
-Get-WindowsCapability -Name RSAT* -Online | Select-Object -Property DisplayName, State
+#write-host "Verify Active Directory tools are installed"
+#Get-WindowsCapability -Name RSAT* -Online | Select-Object -Property DisplayName, State
