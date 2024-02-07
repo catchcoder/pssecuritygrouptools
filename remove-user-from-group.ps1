cf618d6c65c99b15b41a9a49d4b2c48b4eebb063
Chris Hawkins <catchcoder@users.noreply.github.com>
2024-02-07 12:41:03 +0000

    Update remove-user-from-group.ps1

--- remove-user-from-group.ps1
+++ remove-user-from-group.ps1
@@ -1,11 +1,14 @@
-#Requires -Version 6
-#Requires -Modules ActiveDirectory
+# Requires -Version 6
+# Requires -Modules ActiveDirectory
+
 $param1 = $args[0]
 $param2 = $args[1]
 $activedirectoryname = "ads.bris.ac.uk"
 $scriptname = $MyInvocation.MyCommand.name
 $psv = $PSVersionTable.PSversion
+
 Write-Host -ForegroundColor blue "PS $psv $scriptname Version 1.0"
+
 if ($param1 -contains "*" -or $param2 -contains "*") { $param1 = ""; $param2 = "" }
 if ( $param1 -And $param2 ) {
     Write-Host "Remove user ""$param1"" from security group ""$param2"""
