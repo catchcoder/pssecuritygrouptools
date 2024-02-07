f86b9baae197d80d864f917b78e3bc4377344cd0
Chris Hawkins <catchcoder@users.noreply.github.com>
2024-02-07 12:23:06 +0000

    reformatted list-users-in-group.ps1

--- list-users-in-group.ps1
+++ list-users-in-group.ps1
@@ -1,14 +1,20 @@
-#Requires -Version 6
-#Requires -Modules ActiveDirectory
+# Requires -Version 6
+# Requires -Modules ActiveDirectory
+
 $param1 = $args[0]
 $activedirectoryname = "ads.bris.ac.uk"
 $scriptname = $MyInvocation.MyCommand.name
 $psv = $PSVersionTable.PSversion
+
 Write-Host -ForegroundColor blue "PS $psv $scriptname Version 1.0"
-if ($param1 -contains "*") { $param1 = "" }
-if ( $param1 ) {
+
+if ($param1 -contains "*") {
+    $param1 = ""
+}
+
+if ($param1) {
     Write-Host "Searching..."
     Get-ADGroupMember -Identity "$param1" -Server $activedirectoryname | Select-Object name
-} else{
-    Write-Host -ForegroundColor green "Usage ./$scriptname 'ad-security-group'"
+} else {
+    Write-Host -ForegroundColor green "Usage: ./$scriptname 'ad-security-group'"
 }
