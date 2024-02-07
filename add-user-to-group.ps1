d65a442bafffdbf806076225ee8f7ce47bb3961b
Chris Hawkins <catchcoder@users.noreply.github.com>
2024-02-07 12:18:43 +0000

    reformatted add-user-to-group.ps1

--- add-user-to-group.ps1
+++ add-user-to-group.ps1
@@ -1,20 +1,23 @@
-#Requires -Version 6
-#Requires -Modules ActiveDirectory
 $param1 = $args[0]
 $param2 = $args[1]
 $activedirectoryname = "ads.bris.ac.uk"
 $scriptname = $MyInvocation.MyCommand.name
 $psv = $PSVersionTable.PSversion
+
 Write-Host -ForegroundColor blue "PS $psv $scriptname Version 1.0"
-if ($param1 -contains "*" -or $param2 -contains "*") { $param1 ="";$param2="" }
-if ( $param1 -And $param2 ) {
-    Write-Host "Add user ""$param1"" to security group ""$param2"""
+
+if ($param1 -contains "*" -or $param2 -contains "*") {
+    $param1 = ""
+    $param2 = ""
+}
+
+if ($param1 -and $param2) {
+    Write-Host "Adding user ""$param1"" to security group ""$param2"""
     $yn = Read-Host -prompt "Press Y to continue or any other key to cancel"
-    if ($yn -eq "y" -or $yn -eq "Y" ) {
-         Add-ADGroupMember -Identity  "$param2" -Members "$param1" -Server $activedirectoryname
-        }
+    if ($yn -eq "y" -or $yn -eq "Y") {
+        Add-ADGroupMember -Identity "$param2" -Members "$param1" -Server $activedirectoryname
     }
-else {
-    Write-Host -ForegroundColor green "Usage ./$scriptname domain-user-name 'ad-security-group'"
+} else {
+    Write-Host -ForegroundColor green "Usage: ./$scriptname domain-user-name 'ad-security-group'"
 }
 
