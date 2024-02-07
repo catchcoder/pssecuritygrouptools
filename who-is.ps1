8737e6ed01b648cf4a173553caf2f18b5dfdcd24
Chris Hawkins <catchcoder@users.noreply.github.com>
2024-02-07 12:25:10 +0000

    reformatted who-is.ps1

--- who-is.ps1
+++ who-is.ps1
@@ -1,11 +1,17 @@
 #Requires -Version 6
 #Requires -Modules ActiveDirectory
+
 $param1 = $args[0]
 $activedirectoryname = "ads.bris.ac.uk"
 $scriptname = $MyInvocation.MyCommand.name
 $psv = $PSVersionTable.PSversion
+
 Write-Host -ForegroundColor blue "PS $psv $scriptname Version 1.0"
-if ($param1 -contains "*") { $param1 = "" }
+
+if ($param1 -contains "*") { 
+    $param1 = "" 
+}
+
 if ( $param1 ) {
     Write-Host "Searching..."
     Get-ADUser -Identity "$param1" -Server $activedirectoryname
